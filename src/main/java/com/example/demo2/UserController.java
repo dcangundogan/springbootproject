e2042a51a68ca8fbbb3a373acd86964fc2bd85c5
Duygu Can Gündoğan <93220645+dcangundogan@users.noreply.github.com>
2024-07-09 13:33:03 +0300

    a

--- src/main/java/com/example/demo2/UserController.java
+++ src/main/java/com/example/demo2/UserController.java
@@ -17,25 +17,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/{id}")
     public Optional<UserDto> getUserById(@PathVariable UUID id) {
         return userService.getUserById(id);
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping
     public UserDto createUser(@RequestBody UserDto userDto){
         return userService.saveUser(userDto);
 
 
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @PutMapping("/{id}")
     public UserDto updateUser(@RequestBody UserDto userAtt, @PathVariable UUID id){
         return userService.updateUser(id,userAtt);
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable UUID id){
         userService.deleteUser(id);
