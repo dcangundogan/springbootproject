a23d63de7fcd5ae4679e7d6b074387c879fa553f
Duygu Can Gündoğan <93220645+dcangundogan@users.noreply.github.com>
2024-07-09 13:23:59 +0300

    a

--- src/main/java/com/example/demo2/auth/SpringSecurityConfig.java
+++ src/main/java/com/example/demo2/auth/SpringSecurityConfig.java
@@ -2,26 +2,26 @@ package com.example.demo2.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SpringSecurityConfig  {
+    private final MyAppAuthencationProvider authenticationProvider;
+
+
+
+    public SpringSecurityConfig(MyAppAuthencationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;}
 
     @Bean
-    UserDetailsService userDetailsService(){
-        var user = User.withUsername("can").password("123456").authorities("read").build();
-        return new InMemoryUserDetailsManager(user);
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        return http.build();
     }
 
 
