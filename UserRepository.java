a5f49f6fbc0369fc6c3ef0404842ebf7eff3d25e
Duygu Can Gündoğan <93220645+dcangundogan@users.noreply.github.com>
2024-07-09 11:09:59 +0300

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2;
-
-import com.example.demo2.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
