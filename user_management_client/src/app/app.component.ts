fa7032cc07c3a5addcc7f013f3c434a8dae30df3
Duygu Can Gündoğan <93220645+dcangundogan@users.noreply.github.com>
2024-07-19 14:34:23 +0300

    Adding User List for admin Pages

--- user_management_client/src/app/app.component.ts
+++ user_management_client/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
+
   imports: [
     RouterOutlet
   ]
---
+++ user_management_client/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'; // Adjust the path as needed
+import { LoginComponent } from './login/login.component'; // Adjust the path as needed
+import { RegistrationComponent } from './registration/registration.component'; // Adjust the path as needed
+import { UsersComponent } from './users/users.component'; // Adjust the path as needed
+
+import { AuthService } from './services/auth.service'; // Adjust the path as needed
+import { UserService } from './services/user.service'; // Adjust the path as needed
+
+
+
+@NgModule({
+  declarations: [
+
+
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UsersComponent
+  ],
+  providers: [AuthService, UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
